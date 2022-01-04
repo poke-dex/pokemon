932b89aec89a54a4560c36f9212fdf1a444b1de4
Imtiaz <imtiaz.rashid.89@gmail.com>
2022-01-04 17:31:12 -0500

    added a namespace object and called the init method. returned a list of types of pokemon.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
-console.log("hi")
+// Pseudo code
+
+// Create a namespace object
+
+
+const pokeDex = {};
+
+// - create the init method on namespace object
+
+pokeDex.init = () => {
+    // call our getData method
+    pokeDex.getTypeList();
+}
+
+pokeDex.getTypeList = () => {
+    const url = new URL('https://pokeapi.co/api/v2/type')
+    
+    fetch(url)
+        .then((results) => {
+            return results.json();
+        })
+        .then((data) => {
+            console.log(data)
+        })
+}
+
+// create a getData method to request the correct info from the api
+// damage_relations
+// - double_damage_from
+// - double_damage_to
+
+// call the init method at the end
+
+pokeDex.init();
